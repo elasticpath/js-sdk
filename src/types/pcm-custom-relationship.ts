a6ba38a0cc9138ed12df3cc30fbc48272e1ab95a
George Botzakis <georgios.botzakis@elasticpath.com>
2024-09-13 15:50:44 +0300

    feat: Added PCM Custom Relationship endpoint for attaching/detaching custom relationships to/from products, getting custom relationships of a product, attaching/detaching products via a product custom relationship and fetching custom relationship  product data and IDs

---
+++ src/types/pcm-custom-relationship.ts
@@ -0,0 +1,119 @@
+/**
+ * Product Custom Relationships
+ */
+import { Identifiable, ResourceList } from './core'
+import {
+  CustomRelationshipsListResponse
+} from './custom-relationships'
+import { PcmProduct } from './pcm'
+
+export interface PcmProductEntry extends Identifiable {
+  type: 'product'
+}
+
+// TODO -- Entry type of attach/detach endpoint data array field
+export interface CustomRelationshipEntry {
+  type: 'custom-relationship'
+  slug: string
+}
+
+export interface PcmRelatedProductResponse<T> extends ResourceList<T> {
+  meta: {
+    results: {
+      total: number
+    }
+  }
+}
+
+export interface NonAssociatedProductEntry extends Identifiable {
+    details: string
+}
+
+export interface ProductAssociationResponse {
+  meta: {
+    associated_products: string[]
+    products_not_associated: NonAssociatedProductEntry[]
+    owner: 'organization' | 'store'
+    timestamps: {
+      created_at: string
+      updated_at: string
+    }
+  }
+}
+
+// TODO - Update attach/detach endpoint body types after api bug fix
+export interface PcmCustomRelationshipEndpoint {
+  endpoint: 'custom-relationships'
+
+  /**
+   * Get all of a product's custom relationships
+   * @param productId
+   */
+  All(productId: string): Promise<CustomRelationshipsListResponse>
+
+  /**
+   * Attach a custom relationship to a product
+   * @param productId
+   * @param body
+   */
+  AttachCustomRelationship(
+    productId: string,
+    body: CustomRelationshipEntry
+  ): Promise<CustomRelationshipsListResponse>
+
+  /**
+   * Detach a custom relationship from a product
+   * @param productId
+   * @param body
+   */
+  DetachCustomRelationship(
+    productId: string,
+    body: CustomRelationshipEntry[]
+  ): Promise<void>
+
+  /**
+   * Associate a product with other products under a custom relationship
+   * @param productId
+   * @param customRelationshipSlug
+   * @param productsToAssociate
+   * @returns
+   */
+  AssociateProductsToCustomRelationship(
+    productId: string,
+    customRelationshipSlug: string,
+    productsToAssociate: PcmProductEntry[]
+  ): Promise<ProductAssociationResponse>
+
+  /**
+   * Dissociate related products from a product under a custom relationship
+   * @param productId
+   * @param customRelationshipSlug
+   * @param productsToDissociate
+   */
+  DissociateProductsFromCustomRelationship(
+    productId: string,
+    customRelationshipSlug: string,
+    productsToDissociate: PcmProductEntry[]
+  ): Promise<void>
+
+  /**
+   * Get all related products of a product under a custom relationship
+   * @param productId
+   * @param customRelationshipSlug
+   */
+  GetProductsForCustomRelationship(
+    productId: string,
+    customRelationshipSlug: string
+  ): Promise<PcmRelatedProductResponse<PcmProduct>>
+
+  /**
+   * Get all IDs of a product's related products under a custom relationship
+   * @param productId
+   * @param customRelationshipSlug
+   *  TODO CHECK RETURN VALUE
+   */
+  GetProductIdsForCustomRelationship(
+    productId: string,
+    customRelationshipSlug: string
+  ): Promise<PcmRelatedProductResponse<PcmProductEntry>>
+}
