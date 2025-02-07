433dab62dd9933f28944974620a952bd6b4b695b
Ella Nan <38847123+ellanan@users.noreply.github.com>
2025-02-07 09:50:56 -0500

    !fix: support top level “and” with nested “or” in filter (#54)

---
+++ src/utils/formatQueryString.js
@@ -0,0 +1,49 @@
+function formatFilterString(type, filter) {
+    const filterStringArray = Object.keys(filter).map(key => {
+      const value = filter[key]
+      let queryString = `${key},${value}`
+  
+      if (Array.isArray(value)) {
+        queryString = `${key},${value.join(',')}`
+      } else if (typeof value === 'object' && value !== null) {
+        queryString = Object.keys(value)
+          .map(attr => `${key}.${attr},${value[attr]}`)
+          .join(':')
+      }
+      return `${type}(${queryString})`
+    })
+  
+    return filterStringArray.join(':')
+  }
+  
+  
+
+export function formatQueryString(key, value) {
+  if (key === 'limit' || key === 'offset') {
+    return `page${value}`
+  }
+
+  if (key === 'filter') {
+    const filterValues = []
+    
+    // Handle 'or' conditions if they exist
+    if (value.or && Array.isArray(value.or)) {
+      const orQueries = value.or.map(filterGroup => Object.keys(filterGroup)
+        .map(filterType => formatFilterString(filterType, filterGroup[filterType]))
+        .join(':')
+      )
+      filterValues.push(`(${orQueries.join('|')})`)
+    }
+
+    // Handle other filter types
+    Object.keys(value).forEach(filter => {
+      if (filter !== 'or') {
+        filterValues.push(formatFilterString(filter, value[filter]))
+      }
+    })
+
+    return `${key}=${filterValues.join(':')}`
+  }
+
+  return `${key}=${value}`
+}
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,6 +4,7 @@ import {
   DEFAULT_CREDENTIALS_KEY,
   DEFAULT_CURRENCY_KEY
 } from './constants'
+import { formatQueryString } from './formatQueryString'
 
 export function buildRelationshipData(type, ids, typeModifier = underscore) {
   let data = []
@@ -87,49 +88,6 @@ export function parseJSON(response) {
       })
     })
   })
-}
-
-function formatFilterString(type, filter) {
-  const filterStringArray = Object.keys(filter).map(key => {
-    const value = filter[key]
-    let queryString = `${key},${value}`
-
-    if (Array.isArray(value)) {
-      queryString = `${key},${value.join(',')}`
-    } else if (typeof value === 'object' && value !== null) {
-      queryString = Object.keys(value)
-        .map(attr => `${key}.${attr},${value[attr]}`)
-        .join(':')
-    }
-    return `${type}(${queryString})`
-  })
-
-  return filterStringArray.join(':')
-}
-
-function formatQueryString(key, value) {
-  if (key === 'limit' || key === 'offset') {
-    return `page${value}`
-  }
-
-  if (key === 'filter') {
-    if (value.or && Array.isArray(value.or)) {
-      const orQueries = value.or.map(filterGroup =>
-        Object.keys(filterGroup)
-          .map(filterType => formatFilterString(filterType, filterGroup[filterType]))
-          .join(':')
-      )
-      return `${key}=(${orQueries.join('|')})`
-    }
-
-    const filterValues = Object.keys(value).map(filter =>
-      formatFilterString(filter, value[filter])
-    )
-
-    return `${key}=${filterValues.join(':')}`
-  }
-
-  return `${key}=${value}`
 }
 
 function buildQueryParams({ includes, sort, limit, offset, filter, useTemplateSlugs}) {
